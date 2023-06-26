e08b7017c51a282dadf1d7eb02069683b149bc28
HannahMidd <hmiddleton1017@gmail.com>
2023-06-26 10:36:04 -0400

    adding code to apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const store = require('../db/store');
+
+// The /api/notes will get (respond with) the all notes
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(error));
+});
+
+// Post the notes to the page
+router.post('/notes', (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
+
+// Delete a note based on the ID field (each note has unique ID)
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+});
+
+// Export the 'router' object from module
+module.exports = router;
